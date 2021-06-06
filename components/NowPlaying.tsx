766bcad45ce08547f7e8e6413d8e7fc62badcbce
Wilmer Fajardo <wilmerjfl3@gmail.com>
2021-06-06 03:39:27 -0300

    Update NowPlaying.tsx

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -31,7 +31,7 @@ export const Player: React.FC<Props> = ({
             img:not([src]) {
               content: url("data:image/gif;base64,R0lGODlhAQABAPAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
               border-radius: 6px;
-              background: #00B7FC;
+              background: #FFF;
               border: 1px solid #e1e4e8;
             }
 
